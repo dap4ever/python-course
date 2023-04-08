5167435719999e2a675e9b0b54bb7399cab8f7de
Danilo Alves <dap4perez@gmail.com>
2023-04-08 16:05:43 -0300

    frits commit

---
+++ ex002.py
@@ -0,0 +1,7 @@
+print ('Olá, tudo bem? Vamos começar?')
+
+dia = input ('Digite o dia do seu nascimento')
+mes = input ('Digite o mês do seu nascimento')
+ano = input ('Digite o ano do seu nascimento')
+
+print ('Você nasceu no dia ', dia, 'de', mes, 'de', ano,'. Estou correto? ')
---
+++ ex003.py
@@ -0,0 +1,4 @@
+var1 = int(input('primeiro numero '))
+var2 = int(input('segundo numero '))
+soma = (var1) + (var2)
+print('O resultado da soma entre {} e {} é igual: {}'.format(var1,var2,soma) )
